159821720e8caabe59343295529577471e9384f5
IfalEXZ <ifaltampan69@gmail.com>
2024-12-03 00:51:09 +0800

    refactor: improve UserDropdown component structure and enhance user image handling

--- src/components/user-dropdown.tsx
+++ src/components/user-dropdown.tsx
@@ -1,44 +1,56 @@
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
-import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components' 
 
-interface iAppProps {
-    userImage: string | null;
+interface UserDropdownProps {
+  userImage: string | null;
 }
 
-export function UserDropdown( userImage: iAppProps ) {
+export function UserDropdown({ userImage }: UserDropdownProps) {
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger>
-            <div className="rounded-full border px-2 py-2 lg:px-4 lg:px-4 flex items-center gap-x-3">
-                <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5"/>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border px-2 py-2 lg:px-4 lg:px-4 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
 
-                <img src="https://freesvg.org/img/abstract-user-flat-4.png" alt="user avatar" className="rounded-full h-8 w-8 hidden lg:block" />
-            </div>
-        </DropdownMenuTrigger>
+          <img
+            src={
+              userImage ?? "https://freesvg.org/img/abstract-user-flat-4.png"
+            }
+            alt="user avatar"
+            className="rounded-full h-8 w-8 hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
 
-        <DropdownMenuContent align="end" className="w-[200px]">
-            <DropdownMenuItem>
-                <Link href="/create" className="w-full">
-                    Create Post
-                </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-                <Link href="/r/create" className="w-full">
-                    Create Community
-                </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-                <Link href="/r/settings" className="w-full">
-                    Settings
-                </Link>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem>
-                <LogoutLink className="w-full">Logout</LogoutLink>
-            </DropdownMenuItem>
-        </DropdownMenuContent>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        <DropdownMenuItem>
+          <Link href="/create" className="w-full">
+            Create Post
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/r/create" className="w-full">
+            Create Community
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/r/settings" className="w-full">
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogoutLink className="w-full">Logout</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
