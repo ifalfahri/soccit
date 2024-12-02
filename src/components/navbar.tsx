fa5c6f69c941a5d706b6e80b16d8b908f151c4af
IfalEXZ <ifaltampan69@gmail.com>
2024-12-02 23:53:19 +0800

    feat: enhance navbar with authentication buttons and update imports

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import SoccitText from "../../public/logo-name.svg";
 import SoccitMobile from "../../public/soccit-full.svg";
-import { ThemeToggle } from "./theme-toggle";
+import { ThemeToggle } from "@/components/theme-toggle";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
 
 export function Navbar() {
   return (
-    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between flex-row">
+    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
       <Link href="/" className="flex items-center gap-x-3">
         <Image
           src={SoccitMobile}
@@ -19,8 +21,11 @@ export function Navbar() {
           className="h-9 w-fit hidden lg:block"
         />
       </Link>
-
+      <div className="flex items-center gap-x-4">
       <ThemeToggle />
+      <Button variant="secondary">Sign Up</Button>
+      <Button>Login</Button>
+      </div>
     </nav>
   );
 }
